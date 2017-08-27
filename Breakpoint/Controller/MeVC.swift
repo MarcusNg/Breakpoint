a04e7c03bc1d84329dd622c6fc4cf0aacec43551
Marcus Ng <marcuslistanng@gmail.com>
2017-08-27 02:24:34 -0400

    MeVC

---
+++ Breakpoint/Controller/MeVC.swift
@@ -0,0 +1,27 @@
+//
+//  MeVC.swift
+//  Breakpoint
+//
+//  Created by Marcus Ng on 8/27/17.
+//  Copyright © 2017 Marcus Ng. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func signOutBtnPressed(_ sender: Any) {
+        
+    }
+    
+}
